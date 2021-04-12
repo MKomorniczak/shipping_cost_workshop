b215242f2a92f614b5799dcfe71957e8cdfa6800
retaz <retazz1991@gmail.com>
2021-04-12 10:25:23 +0200

    some errors, cannot run the app

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -1,4 +1,30 @@
 package se.lexicon.shipping_cost.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import se.lexicon.shipping_cost.entity.Box;
+
+import javax.validation.Valid;
+
+@Controller
 public class ShippingController {
+
+    @GetMapping("/AddBoxForm")
+    public String addBoxForm(Model model){
+        Box box = new Box();
+        model.addAttribute("box", box);
+        return "AddBoxForm";
+    }
+
+    @PostMapping("/register")
+    public String regButton(@Valid @ModelAttribute("box") Box box, BindingResult bindingResult) {
+        System.out.println(box.toString());
+        if (bindingResult.hasErrors())
+            return "AddBoxForm";
+        return "redirect:/";
+    }
 }
--- src/main/java/se/lexicon/shipping_cost/entity/Box.java
+++ src/main/java/se/lexicon/shipping_cost/entity/Box.java
@@ -10,7 +10,7 @@ public class Box {
     @Id
     String id;
     @Size(min = 4, max = 20, message = "name length should be between 4 and 20")
-    @Valid
+    //@Valid
     String name;
     @NotNull(message = "country should not be null")
     String country;
@@ -26,4 +26,91 @@ public class Box {
     LocalDateTime createDate;
 
     boolean status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getWeightType() {
+        return weightType;
+    }
+
+    public void setWeightType(String weightType) {
+        this.weightType = weightType;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                ", type='" + type + '\'' +
+                ", cost=" + cost +
+                ", weight=" + weight +
+                ", weightType='" + weightType + '\'' +
+                ", createDate=" + createDate +
+                ", status=" + status +
+                '}';
+    }
 }
