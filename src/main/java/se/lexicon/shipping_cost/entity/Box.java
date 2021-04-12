7c2975c1cb72afad1f9a50dee7a568a5326adb98
retaz <retazz1991@gmail.com>
2021-04-12 10:48:14 +0200

    errors after pressing Register button

--- src/main/java/se/lexicon/shipping_cost/entity/Box.java
+++ src/main/java/se/lexicon/shipping_cost/entity/Box.java
@@ -1,26 +1,32 @@
 package se.lexicon.shipping_cost.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
+@Entity
 public class Box {
     @Id
     String id;
     @Size(min = 4, max = 20, message = "name length should be between 4 and 20")
-    //@Valid
+    @Valid
     String name;
     @NotNull(message = "country should not be null")
+    @Valid
     String country;
     @NotNull(message = "type should not be null")
+    @Valid
     String type;
 
     double cost;
     @Size(min = 1, message = "weight should not be less than 1")
+    @Valid
     double weight;
     @NotNull(message = "weight type should not be null")
+    @Valid
     String weightType;
 
     LocalDateTime createDate;
